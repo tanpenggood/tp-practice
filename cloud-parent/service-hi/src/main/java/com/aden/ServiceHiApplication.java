7f60a31f2c7f663f1ba2646c3a4df8501b876132
tanpeng <tanpeng@yadingdata.com>
2019-12-04 12:49:47 +0800

    add service-hi

---
+++ cloud-parent/service-hi/src/main/java/com/aden/ServiceHiApplication.java
@@ -0,0 +1,27 @@
+package com.aden;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class ServiceHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/hi")
+    public String hi(@RequestParam(value = "name") String name) {
+        return "hi " + name + ",i am from port:" + port;
+    }
+}
