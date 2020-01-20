d692ec0af6656197c20f172006266dcf7fdb7585
tanpenggood <tanpengswpu@163.com>
2020-01-20 10:27:31 +0800

    feign新增fallbackFactory

--- cloud-parent/service-feign/src/main/java/com/aden/fallback/ScheduleServiceHiHystrix.java
+++ cloud-parent/service-feign/src/main/java/com/aden/fallback/ScheduleServiceHiHystrix.java
@@ -4,7 +4,9 @@ import com.aden.feign.ScheduleServiceHi;
 import org.springframework.stereotype.Component;
 
 /**
- * @description:
+ * @description: feign统一熔断处理
+ * fallback
+ *  仅提供服务降级，不能查看服务降级的具体原因
  * @author: tanpeng
  * @date: 2019-12-04 11:04
  * @version: 1.0.0
@@ -12,7 +14,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduleServiceHiHystrix implements ScheduleServiceHi {
 
-    @Override
     public String sayHiFromClientOne(String name) {
         return "sorry " + name;
     }
