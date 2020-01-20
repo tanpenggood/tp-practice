0923ce72fe94e5d5790782611f7daf68e301573f
tanpeng <tanpeng@yadingdata.com>
2020-01-20 10:28:07 +0800

    zuul过滤器重命名

---
+++ cloud-parent/service-zuul/src/main/java/com/aden/filter/OrderContant.java
@@ -0,0 +1,14 @@
+package com.aden.filter;
+
+/**
+ * @description:
+ * @author: tanpeng
+ * @date: 2019-12-06 9:19
+ * @version: 1.0.0
+ */
+public class OrderContant {
+
+    public static final int FIRST_FILTER = 0;
+    public static final int SECOND_FILTER = 1;
+    public static final int THIRD_FILTER = 2;
+}
