4a38fdaf0d7560771cbcdb3268102aa3ff6d7f49
tanpeng <tpswpu@yeah.net>
2020-01-29 20:34:23 +0800

    列表&keys&JSXDemo

--- learn-react/src/components/Demo7NumberList.js
+++ learn-react/src/components/Demo7NumberList.js
@@ -3,7 +3,7 @@ import React from 'react'
 class NumberList extends React.Component{
 
     /**
-     * @description: 1、初始化
+     * @description: 1、声明并初始化
      * @author: tanpeng
      * @date : 2020-01-29 12:42
      * @version: v1.0.0
@@ -34,6 +34,8 @@ class NumberList extends React.Component{
     getListItems() {
         const numbers = this.state.numbers
         const listItems = numbers.map((num) =>
+            // Keys 可以在 DOM 中的某些元素被增加或删除的时候帮助 React 识别哪些元素发生了变化。
+            // 因此你应当给数组中的每一个元素赋予一个确定的标识key。
             <li key={num.toString()}>{num * 2}</li>
         )
         return listItems
