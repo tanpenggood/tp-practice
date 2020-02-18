39601ba97a8dbb78d842ab6bb349c62191a9200d
tanpeng <tanpeng@yadingdata.com>
2020-02-18 15:54:58 +0800

    add ce4j

---
+++ ce4j/src/main/java/com/aden/biz/Git.java
@@ -0,0 +1,22 @@
+package com.aden.biz;
+
+/**
+ * @description:
+ * @author: tanpeng
+ * @date: 2020-02-18 11:47
+ * @version: 1.0.0
+ */
+public interface Git {
+
+    String ROOT_DIR = "e:/code/aden/Z-jenkins/test";
+
+    void clone2(String gitURL);
+
+    void pull(String destDir);
+
+    void checkout(String targetProject, String targetBranch);
+
+    void push();
+
+    void template();
+}
