ca9f8a2aa68445e4fe61ffd1efbc739d7c1a674e
tanpenggood <tanpengswpu@163.com>
2020-03-19 12:49:40 +0800

    项目搭建 boot：1.5.20.RELEASE cloud：Dalston.SR2

---
+++ itplh-security/itplh-security-demo/src/main/java/com/itplh/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.itplh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: tanpeng
+ * @date: 2020-03-19 11:26
+ * @version: v1.0.0
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring security!";
+    }
+}
