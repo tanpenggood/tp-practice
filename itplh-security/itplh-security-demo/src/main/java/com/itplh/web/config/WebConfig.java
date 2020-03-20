7efc1064686b99adff719035d8975776901b59fb
tanpenggood <tanpengswpu@163.com>
2020-03-20 15:16:39 +0800

    增加自定义线程池 ThreadPoolTaskExecutor

--- itplh-security/itplh-security-demo/src/main/java/com/itplh/web/config/WebConfig.java
+++ itplh-security/itplh-security-demo/src/main/java/com/itplh/web/config/WebConfig.java
@@ -5,10 +5,13 @@ import com.itplh.web.interceptor.TimeInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.Arrays;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @description:
@@ -19,11 +22,6 @@ import java.util.Arrays;
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TimeInterceptor());
-    }
-
     @Bean
     public FilterRegistrationBean timeFilter() {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
@@ -31,5 +29,28 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
         return filterRegistrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new TimeInterceptor());
+    }
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.setTaskExecutor(threadPoolTaskExecutor());
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor threadPool = new ThreadPoolTaskExecutor();
+        threadPool.setCorePoolSize(5);
+        threadPool.setMaxPoolSize(Runtime.getRuntime().availableProcessors() + 1);
+        threadPool.setKeepAliveSeconds(60);
+        threadPool.setQueueCapacity(512);
+        threadPool.setThreadNamePrefix("aden-tp-");
+        threadPool.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        threadPool.initialize();
+        return threadPool;
     }
 }
