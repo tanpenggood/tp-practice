93f6a07527dfabee0927dde3d4d83b5337c4e15d
tanpenggood <tanpengswpu@163.com>
2020-03-20 11:35:39 +0800

    增加testUploadSuccess测试用例 增加FileController

---
+++ itplh-security/itplh-security-demo/src/main/java/com/itplh/web/controller/FileController.java
@@ -0,0 +1,61 @@
+package com.itplh.web.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * @description:
+ * @author: tanpeng
+ * @date: 2020-03-20 10:55
+ * @version: v1.0.0
+ */
+@RestController
+@RequestMapping("/file")
+public class FileController {
+
+    private String folder = "/Users/tanpeng/tp-code/tp-practice/itplh-security/itplh-security-demo/src/main/java/com/itplh/web/controller";
+
+    @PostMapping
+    public FileInfo upload(MultipartFile file) throws IOException {
+        System.out.println(file.getName());
+        System.out.println(file.getOriginalFilename());
+        System.out.println(file.getSize());
+
+        File localFile = new File(folder, file.getOriginalFilename());
+        file.transferTo(localFile);
+        return new FileInfo(localFile.getAbsolutePath());
+    }
+
+    @GetMapping("/{id}")
+    public void download(@PathVariable String id, HttpServletResponse response) throws IOException {
+        try (
+                OutputStream out = response.getOutputStream();
+                InputStream in = new FileInputStream(new File(folder, id + ".txt"));
+        ) {
+            response.setContentType("application/x-download");
+            response.addHeader("Content-Disposition", "attachment;filename=" + id + ".txt");
+            IOUtils.copy(in, out);
+            out.flush();
+        }
+    }
+}
+
+@Data
+@AllArgsConstructor
+class FileInfo {
+    private String path;
+}
--- itplh-security/itplh-security-demo/src/test/java/com/itplh/web/TestUserController.java
+++ itplh-security/itplh-security-demo/src/test/java/com/itplh/web/TestUserController.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -112,6 +114,15 @@ public class TestUserController {
     public void testDeleteSuccess() throws Exception {
         mockMvc.perform(delete("/user/1").contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUploadSuccess() throws Exception {
+        String result = mockMvc.perform(fileUpload("/file")
+                .file(new MockMultipartFile("file", "aaa.txt", "multipart/form-data", "hello upload".getBytes("UTF-8"))))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        System.out.println(result);
     }
 
 }
