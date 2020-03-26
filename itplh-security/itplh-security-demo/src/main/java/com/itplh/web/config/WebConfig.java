d6084852aa44f8b9a59a8133c46813b316bb79e0
tanpeng <tpswpu@yeah.net>
2020-03-26 16:04:52 +0800

    重写configureAsyncSupport，配置线程池

--- itplh-security/itplh-security-demo/src/main/java/com/itplh/web/config/WebConfig.java
+++ itplh-security/itplh-security-demo/src/main/java/com/itplh/web/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -35,13 +36,15 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addInterceptor(new TimeInterceptor());
     }
 
-
     @Autowired
-    private ThreadPoolConfig threadPoolConfig;
+    private ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
     @Override
     public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-        configurer.setTaskExecutor(threadPoolConfig.threadPoolTaskExecutor());
+        // 指定自定义线程池
+        configurer.setTaskExecutor(threadPoolTaskExecutor);
+        // 超时时间5分钟
+        configurer.setDefaultTimeout(5L * 60 * 1000);
     }
 
 }
