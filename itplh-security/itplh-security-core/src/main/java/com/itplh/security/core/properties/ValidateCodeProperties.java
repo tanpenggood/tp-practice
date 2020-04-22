ca16289a90cd86f6b8de6019a759b0c112f11cb7
tanpeng <tpswpu@yeah.net>
2020-04-22 21:07:11 +0800

    增加验证码配置

---
+++ itplh-security/itplh-security-core/src/main/java/com/itplh/security/core/properties/ValidateCodeProperties.java
@@ -0,0 +1,16 @@
+package com.itplh.security.core.properties;
+
+import lombok.Data;
+
+/**
+ * @description:
+ * @author: tanpeng
+ * @date: 2020-04-22 20:34
+ * @version: v1.0.0
+ */
+@Data
+public class ValidateCodeProperties {
+
+    private ImageCodeProperties image = new ImageCodeProperties();
+
+}
