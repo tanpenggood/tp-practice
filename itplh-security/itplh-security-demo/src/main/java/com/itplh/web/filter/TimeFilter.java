38c359dd664f2d20848851997d0f6a272f50b7b5
tanpeng <tanpeng@yadingdata.com>
2020-04-22 13:52:18 +0800

    新增@Profiles("cost-time")

--- itplh-security/itplh-security-demo/src/main/java/com/itplh/web/filter/TimeFilter.java
+++ itplh-security/itplh-security-demo/src/main/java/com/itplh/web/filter/TimeFilter.java
@@ -2,12 +2,7 @@ package com.itplh.web.filter;
 
 import lombok.extern.slf4j.Slf4j;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import java.io.IOException;
 
 /**
