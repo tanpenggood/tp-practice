aa7eac6aa927e38e9cdf4f91c1b3cd71479d3841
tanpenggood <tanpengswpu@163.com>
2020-04-22 13:55:52 +0800

    实现自定义登录页(框架级)、自定义登录页(用户级)

---
+++ itplh-security/itplh-security-core/src/main/java/com/itplh/security/core/properties/BrowserProperties.java
@@ -0,0 +1,16 @@
+package com.itplh.security.core.properties;
+
+import lombok.Data;
+
+/**
+ * @description:
+ * @author: tanpeng
+ * @date: 2020-04-22 12:51
+ * @version: 1.0.0
+ */
+@Data
+public class BrowserProperties {
+
+    private String loginPage = "/sign-in.html";
+
+}
