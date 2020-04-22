93e3aabcfd0071853ea6307ad1c4691c1e466a6d
tanpenggood <tanpengswpu@163.com>
2020-04-22 20:57:55 +0800

    重命名枚举类LoginResponseTypeEnum

--- itplh-security/itplh-security-browser/src/main/java/com/itplh/security/browser/authentication/ItplhAuthenticationSuccessHandler.java
+++ itplh-security/itplh-security-browser/src/main/java/com/itplh/security/browser/authentication/ItplhAuthenticationSuccessHandler.java
@@ -1,7 +1,7 @@
 package com.itplh.security.browser.authentication;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.itplh.security.core.properties.LoginResponseType;
+import com.itplh.security.core.properties.LoginResponseTypeEnum;
 import com.itplh.security.core.properties.SecurityProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,7 +36,7 @@ public class ItplhAuthenticationSuccessHandler extends SavedRequestAwareAuthenti
 
 		logger.info("登录成功");
 
-		if (LoginResponseType.JSON.equals(securityProperties.getBrowser().getLoginType())) {
+		if (LoginResponseTypeEnum.JSON.equals(securityProperties.getBrowser().getLoginType())) {
 			response.setContentType("application/json;charset=UTF-8");
 			response.getWriter().write(objectMapper.writeValueAsString(authentication));
 		} else {
