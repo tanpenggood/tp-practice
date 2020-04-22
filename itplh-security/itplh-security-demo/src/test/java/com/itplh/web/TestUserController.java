59a3e457a1db9144da6ea7a20a80ad460140e8c7
tanpenggood <tanpengswpu@163.com>
2020-04-22 13:52:39 +0800

    导包优化

--- itplh-security/itplh-security-demo/src/test/java/com/itplh/web/TestUserController.java
+++ itplh-security/itplh-security-demo/src/test/java/com/itplh/web/TestUserController.java
@@ -15,11 +15,7 @@ import org.springframework.web.context.WebApplicationContext;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
