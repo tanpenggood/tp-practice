6cbad244a1f3043559e07fccca73ffbd1850db84
tanpeng <tanpeng@yadingdata.com>
2020-05-07 12:17:36 +0800

    新增接口/me

--- learn-spring-boot/learn-spring-security/src/main/java/com/itplh/security/controller/LoginSuccessController.java
+++ learn-spring-boot/learn-spring-security/src/main/java/com/itplh/security/controller/LoginSuccessController.java
@@ -1,7 +1,10 @@
 package com.itplh.security.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -16,6 +19,12 @@ public class LoginSuccessController {
     @RequestMapping("/login-success")
     public ModelAndView success() {
         return new ModelAndView("login-success");
+    }
+
+    @GetMapping("/me")
+    @ResponseBody
+    public Authentication me(Authentication user) {
+        return user;
     }
 
 }
