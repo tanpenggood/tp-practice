2334c059bf6f5aca89172abb7c5c0497acb11cd5
tanpeng <tanpeng@yadingdata.com>
2020-05-09 15:36:58 +0800

    调整Spring Security的SpEL表达式注释

--- learn-spring-boot/learn-spring-security/src/main/java/com/itplh/security/controller/MethodSecurityPrePostController.java
+++ learn-spring-boot/learn-spring-security/src/main/java/com/itplh/security/controller/MethodSecurityPrePostController.java
@@ -8,15 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @description:
  * Spring Security 支持的所有SpEL表达式如下：
  *      authentication　　	用户认证对象
- *      denyAll　　	结果始终为false
- *      hasAnyRole(list of roles)　　	如果用户被授权指定的任意权限，结果为true
- *      hasRole(role)	如果用户被授予了指定的权限，结果 为true
+ *      hasRole(role)	如果用户被授予了指定的角色，结果为true
+ *      hasAnyRole(list of roles) 如果用户拥有指定的任意角色，结果为true
+ *      hasAuthority(authority) 如果用户被授予了指定的权限，结果为true
+ *      hasAnyAuthority(list of authorities) 如果用户被授予了指定的权限，结果为true
  *      hasIpAddress(IP Address)	用户地址
- *      isAnonymous()　　	是否为匿名用户
- *      isAuthenticated()　　	不是匿名用户
+ *      isAnonymous() 匿名用户，结果为true
+ *      isAuthenticated() 已认证用户，结果为true
  *      isFullyAuthenticated　　	不是匿名也不是remember-me认证
  *      isRememberMe()　　	remember-me认证
  *      permitAll	始终true
+ *      denyAll　　	结果始终为false
  *      principal	用户主要信息对象
  * @author: tanpeng
  * @date: 2020-05-07 11:14
