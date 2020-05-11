b688c81c65e89c622ee14775707bd7772a631852
tanpeng <tanpeng@yadingdata.com>
2020-05-11 16:35:54 +0800

    整合mybatis-plus将房源信息持久化

---
+++ bk-java/src/main/java/com/itplh/bk/mapper/HouseInfoMapper.java
@@ -0,0 +1,15 @@
+package com.itplh.bk.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itplh.bk.model.HouseInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @description:
+ * @author: tanpeng
+ * @date: 2020-05-11 16:11
+ * @version: 1.0.0
+ */
+@Mapper
+public interface HouseInfoMapper extends BaseMapper<HouseInfo> {
+}
