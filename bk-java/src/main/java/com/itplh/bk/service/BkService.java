ed3baa420941c82e8c62c4cb53d5834f833707eb
tanpeng <tanpeng@yadingdata.com>
2020-05-11 14:46:09 +0800

    分环境动态设置totalPage(dev环境时只抓取首页数据)

--- bk-java/src/main/java/com/itplh/bk/service/BkService.java
+++ bk-java/src/main/java/com/itplh/bk/service/BkService.java
@@ -8,7 +8,6 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
