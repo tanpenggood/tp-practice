d90e8c48dedc2f4dea1875cce14b01c4ff8e29a1
tanpenggood <tanpengswpu@163.com>
2020-06-06 16:10:30 +0800

    第三章 Spring5.x基础

---
+++ learn-springboot-bywyf/chapter3-spring5x-basic/src/main/java/com/itplh/com/itplh/section8/DemoLoggerService.java
@@ -0,0 +1,20 @@
+package com.itplh.com.itplh.section8;
+
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: tanpenggood
+ * @since: 2020-06-06 15:10
+ */
+@Service
+public class DemoLoggerService {
+
+    @InjectLogger
+    private Logger logger;
+
+    public void doSomething() {
+        logger.info("通过自定义InjectLoggerAnnotationBeanPostProcessor让注解@InjectLogger注入Logger对象");
+    }
+
+}
---
+++ learn-springboot-bywyf/chapter3-spring5x-basic/src/main/java/com/itplh/com/itplh/section8/JavaConfig.java
@@ -0,0 +1,19 @@
+package com.itplh.com.itplh.section8;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: tanpenggood
+ * @since: 2020-06-06 15:12
+ */
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(DemoLoggerService demoLoggerService) {
+        return args -> demoLoggerService.doSomething();
+    }
+
+}
