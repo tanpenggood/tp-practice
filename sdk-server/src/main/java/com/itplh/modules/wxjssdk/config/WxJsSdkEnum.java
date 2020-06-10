9e320f43845af7fcc548761977497cda6a196dd9
tanpenggood <tanpengswpu@163.com>
2020-06-11 00:35:44 +0800

    增加WxJsSdkEnum存放配置数据

---
+++ sdk-server/src/main/java/com/itplh/modules/wxjssdk/config/WxJsSdkEnum.java
@@ -0,0 +1,18 @@
+package com.itplh.modules.wxjssdk.config;
+
+import lombok.Getter;
+
+@Getter
+public enum WxJsSdkEnum {
+
+    TOKEN("TOKEN"),
+    APPID("APPID"),
+    SECRET("SECRET"),
+    ACCESS_TOKEN_URL_TEMPLDATE("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s");
+
+    private String value;
+
+    WxJsSdkEnum(String value) {
+        this.value = value;
+    }
+}
