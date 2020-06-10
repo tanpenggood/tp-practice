97371d084f98d1b17de9ebc8133c8e3c1fedbce5
tanpenggood <tanpengswpu@163.com>
2020-06-10 14:55:13 +0800

    测试雪花算法生成ID

--- sdk-server/src/main/java/com/itplh/modules/wxjssdk/controller/WxJsSdkController.java
+++ sdk-server/src/main/java/com/itplh/modules/wxjssdk/controller/WxJsSdkController.java
@@ -1,12 +1,15 @@
 package com.itplh.modules.wxjssdk.controller;
 
 import com.itplh.common.Result;
+import com.itplh.common.util.IdWorker;
 import com.itplh.modules.wxjssdk.mapper.WxJsSdkMapper;
 import com.itplh.modules.wxjssdk.pojo.entity.WxJsSdk;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * @author: tanpeng
@@ -18,10 +21,24 @@ public class WxJsSdkController {
 
     @Autowired
     private WxJsSdkMapper wxJsSdkMapper;
+    @Autowired
+    private IdWorker idWorker;
 
     @GetMapping
     public Result get() {
         WxJsSdk wxJsSdk = wxJsSdkMapper.selectById(1);
+        return Result.ok(wxJsSdk);
+    }
+
+    @GetMapping("insert")
+    public Result insert() {
+        long nextId = idWorker.nextId();
+        WxJsSdk wxJsSdk = new WxJsSdk();
+        wxJsSdk.setId(nextId);
+        wxJsSdk.setAccessToken("token" + nextId);
+        wxJsSdk.setTicket("ticket" + nextId);
+        wxJsSdk.setCreateTime(new Date());
+        wxJsSdkMapper.insert(wxJsSdk);
         return Result.ok(wxJsSdk);
     }
 }
