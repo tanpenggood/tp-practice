e3de66ca61a8aefe7027551159abb6def20e10d6
tanpenggood <tanpengswpu@163.com>
2020-06-11 00:51:15 +0800

    启动后输出访问链接

--- sdk-server/src/main/java/com/itplh/SdkServerApplication.java
+++ sdk-server/src/main/java/com/itplh/SdkServerApplication.java
@@ -2,12 +2,25 @@ package com.itplh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class SdkServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SdkServerApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext context = SpringApplication.run(SdkServerApplication.class, args);
+        ConfigurableEnvironment env = context.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        System.out.println("##########################################################################");
+        System.out.println("# SDK SERVER is running! Access URLs:");
+        System.out.printf("# Local: http://localhost:%s\n", port);
+        System.out.printf("# External: http://%s:%s\n", ip, port);
+        System.out.println("##########################################################################");
     }
 
 }
