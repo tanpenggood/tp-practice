d6cde13581f8562e0169170b6af946ee11c1a65a
tanpenggood <tanpengswpu@163.com>
2020-06-14 00:45:45 +0800

    保证表只被创建一次，使用CREATE TABLE IF NOT EXISTS

--- sdk-server/sdk-common/src/main/resources/db/schema.sql
+++ sdk-server/sdk-common/src/main/resources/db/schema.sql
@@ -1,6 +1,4 @@
-DROP TABLE IF EXISTS wx_js_sdk;
-
-CREATE TABLE wx_js_sdk(
+CREATE TABLE IF NOT EXISTS wx_js_sdk(
   id BIGINT NOT NULL COMMENT '雪花ID',
   access_token VARCHAR(512),
   ticket VARCHAR(512),
