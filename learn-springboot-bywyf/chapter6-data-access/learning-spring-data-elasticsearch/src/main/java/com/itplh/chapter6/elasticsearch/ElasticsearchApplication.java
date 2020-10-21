594df313b9bdbc6988a907c13f9bb5dc702e1847
tanpenggood <tanpengswpu@163.com>
2020-10-22 01:00:19 +0800

    feat(learning-spring-data-elasticsearch): Add aggregations query sample.

--- learn-springboot-bywyf/chapter6-data-access/learning-spring-data-elasticsearch/src/main/java/com/itplh/chapter6/elasticsearch/ElasticsearchApplication.java
+++ learn-springboot-bywyf/chapter6-data-access/learning-spring-data-elasticsearch/src/main/java/com/itplh/chapter6/elasticsearch/ElasticsearchApplication.java
@@ -5,9 +5,20 @@ import com.itplh.chapter6.elasticsearch.domain.Child;
 import com.itplh.chapter6.elasticsearch.domain.Gender;
 import com.itplh.chapter6.elasticsearch.domain.Person;
 import com.itplh.chapter6.elasticsearch.repository.PersonRepository;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.filter.ParsedFilter;
+import org.elasticsearch.search.aggregations.bucket.range.ParsedRange;
+import org.elasticsearch.search.aggregations.metrics.avg.ParsedAvg;
+import org.elasticsearch.search.aggregations.metrics.max.ParsedMax;
+import org.elasticsearch.search.aggregations.metrics.min.ParsedMin;
+import org.elasticsearch.search.aggregations.metrics.stats.ParsedStats;
+import org.elasticsearch.search.aggregations.metrics.sum.ParsedSum;
+import org.elasticsearch.search.aggregations.metrics.valuecount.ParsedValueCount;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +26,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
 
@@ -142,6 +154,63 @@ public class ElasticsearchApplication {
             System.out.println("-------------------");
             personPage3.forEach(System.out::println);
             System.out.println("-------------------");
+        };
+    }
+
+    /**
+     * 查询统计
+     *
+     * @author: tanpenggood
+     * @date: 2020-10-22 00:24
+     */
+    @Bean
+    CommandLineRunner aggregateQuery(ElasticsearchOperations elasticsearchOperations) {
+        return args -> {
+            SearchQuery aggregateQuery = new NativeSearchQueryBuilder()
+                    .withIndices("person")
+                    .addAggregation(AggregationBuilders.sum("sumAge").field("age"))
+                    .addAggregation(AggregationBuilders.max("maxAge").field("age"))
+                    .addAggregation(AggregationBuilders.min("minAge").field("age"))
+                    .addAggregation(AggregationBuilders.count("countAge").field("age"))
+                    .addAggregation(AggregationBuilders.avg("avgAge").field("age"))
+                    .addAggregation(AggregationBuilders.stats("ageInfo").field("age"))
+                    .addAggregation(AggregationBuilders.range("ageRange").field("age").addRange(20, 25))
+                    .addAggregation(AggregationBuilders.filter("leftPerson",
+                            QueryBuilders.rangeQuery("age").from(20).to(25))
+                            .subAggregation(AggregationBuilders.sum("leftSum").field("age")))
+                    .build();
+
+            Aggregations aggs = elasticsearchOperations.query(aggregateQuery, SearchResponse::getAggregations);
+
+            double sum = ((ParsedSum) aggs.get("sumAge")).getValue();
+            System.out.println("sum age is " + sum);
+            double max = ((ParsedMax) aggs.get("maxAge")).getValue();
+            System.out.println("max age is " + max);
+            double min = ((ParsedMin) aggs.get("minAge")).getValue();
+            System.out.println("min age is " + min);
+            double avg = ((ParsedAvg) aggs.get("avgAge")).getValue();
+            System.out.println("avg age is " + avg);
+            double count = ((ParsedValueCount) aggs.get("countAge")).getValue();
+            System.out.println("count age is " + count);
+
+            ParsedStats stats = aggs.get("ageInfo");
+            System.out.println("sum age is " + stats.getSumAsString());
+            System.out.println("max age is " + stats.getMaxAsString());
+            System.out.println("min age is " + stats.getMinAsString());
+            System.out.println("avg age is " + stats.getAvgAsString());
+            System.out.println("count age is " + stats.getCount());
+
+
+            ((ParsedRange) aggs.get("ageRange")).getBuckets().forEach(bucket -> {
+                System.out.printf("%s到%s 数量为：%s\n",
+                        bucket.getFrom(),
+                        bucket.getTo(),
+                        bucket.getDocCount());
+            });
+
+            Aggregations filterAggs = ((ParsedFilter) aggs.get("leftPerson")).getAggregations();
+            double leftSum = ((ParsedSum) filterAggs.get("leftSum")).getValue();
+            System.out.println("left sum is " + leftSum);
         };
     }
 
