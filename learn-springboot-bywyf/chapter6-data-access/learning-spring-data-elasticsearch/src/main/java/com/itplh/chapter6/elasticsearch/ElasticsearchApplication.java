f1c87a20cf961bd3b57ebb0f3886041bdf04a715
tanpenggood <tanpengswpu@163.com>
2020-10-22 00:13:08 +0800

    feat(learning-spring-data-elasticsearch): Add SearchQuery sample.

--- learn-springboot-bywyf/chapter6-data-access/learning-spring-data-elasticsearch/src/main/java/com/itplh/chapter6/elasticsearch/ElasticsearchApplication.java
+++ learn-springboot-bywyf/chapter6-data-access/learning-spring-data-elasticsearch/src/main/java/com/itplh/chapter6/elasticsearch/ElasticsearchApplication.java
@@ -5,6 +5,9 @@ import com.itplh.chapter6.elasticsearch.domain.Child;
 import com.itplh.chapter6.elasticsearch.domain.Gender;
 import com.itplh.chapter6.elasticsearch.domain.Person;
 import com.itplh.chapter6.elasticsearch.repository.PersonRepository;
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +15,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -28,6 +33,39 @@ public class ElasticsearchApplication {
         SpringApplication.run(ElasticsearchApplication.class, args);
     }
 
+    /**
+     * 插入数据
+     *
+     * @author: tanpenggood
+     * @date: 2020-10-21 23:57
+     */
+    @Bean
+    CommandLineRunner saveAll(PersonRepository personRepository) {
+        return args -> {
+            personRepository.deleteAll();
+
+            Address address = new Address("chong qing", "chong qing");
+            Collection<Child> children = Arrays.asList(new Child("tyn", Gender.FEMALE), new Child("tbe", Gender.MALE));
+            Person person = new Person("tp", 25, address, children);
+
+            Address address1 = new Address("bei jing", "bei jing");
+            Collection<Child> children1 = Arrays.asList(new Child("aaa", Gender.FEMALE), new Child("bbb", Gender.MALE));
+            Person person1 = new Person("foo", 24, address1, children1);
+
+            Address address2 = new Address("shang hai", "shang hai");
+            Collection<Child> children2 = Arrays.asList(new Child("ccc", Gender.FEMALE), new Child("ddd", Gender.MALE));
+            Person person2 = new Person("bar", 26, address2, children2);
+
+            personRepository.saveAll(Arrays.asList(person, person1, person2));
+        };
+    }
+
+    /**
+     * 查 jpa方式
+     *
+     * @author: tanpenggood
+     * @date: 2020-10-21 23:57
+     */
     @Bean
     CommandLineRunner query(PersonRepository personRepository) {
         return args -> {
@@ -46,22 +84,64 @@ public class ElasticsearchApplication {
         };
     }
 
-    //    @Bean
-    CommandLineRunner saveAll(PersonRepository personRepository) {
+    /**
+     * 查 search方式
+     *
+     * @author: tanpenggood
+     * @date: 2020-10-21 23:57
+     */
+    @Bean
+    CommandLineRunner queryBuilderAndSearchQuery(PersonRepository personRepository) {
         return args -> {
-            Address address = new Address("chong qing", "chong qing");
-            Collection<Child> children = Arrays.asList(new Child("tyn", Gender.FEMALE), new Child("tbe", Gender.MALE));
-            Person person = new Person("tp", 25, address, children);
+            QueryBuilder queryBuilder = QueryBuilders.matchAllQuery();
+            SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                    .withQuery(queryBuilder)
+                    .withPageable(PageRequest.of(0, 3, Sort.by("age")))
+                    .build();
 
-            Address address1 = new Address("bei jing", "bei jing");
-            Collection<Child> children1 = Arrays.asList(new Child("aaa", Gender.FEMALE), new Child("bbb", Gender.MALE));
-            Person person1 = new Person("foo", 24, address1, children1);
+            Page<Person> personPage1 = personRepository.search(queryBuilder, PageRequest.of(0, 3, Sort.by("age")));
+            Page<Person> personPage2 = personRepository.search(searchQuery);
+            System.out.printf("personPage1 总数为：%s 总页数为：%s\n",
+                    personPage1.getTotalElements(),
+                    personPage1.getTotalPages());
+            personPage1.forEach(System.out::println);
+            System.out.printf("personPage2 总数为：%s 总页数为：%s\n",
+                    personPage2.getTotalElements(),
+                    personPage2.getTotalPages());
+            personPage2.forEach(System.out::println);
+        };
+    }
 
-            Address address2 = new Address("shang hai", "shang hai");
-            Collection<Child> children2 = Arrays.asList(new Child("ccc", Gender.FEMALE), new Child("ddd", Gender.MALE));
-            Person person2 = new Person("bar", 26, address2, children2);
+    /**
+     * 查 search方式
+     *
+     * @author: tanpenggood
+     * @date: 2020-10-22 00:12
+     */
+    @Bean
+    CommandLineRunner search(PersonRepository personRepository) {
+        return args -> {
+            SearchQuery queryByAgeRangeAndNameRegex = new NativeSearchQueryBuilder()
+                    .withQuery(QueryBuilders.rangeQuery("age").from(20).to(25))
+                    .withFilter(QueryBuilders.matchQuery("name", "tp"))
+                    .build();
+            SearchQuery fuzzyQuery = new NativeSearchQueryBuilder()
+                    .withQuery(QueryBuilders.fuzzyQuery("address.city", "shang").fuzziness(Fuzziness.AUTO))
+                    .build();
+            SearchQuery anotherFuzzyQuery = new NativeSearchQueryBuilder()
+                    .withQuery(QueryBuilders.matchQuery("address.city", "jing").fuzziness(Fuzziness.AUTO))
+                    .build();
 
-            personRepository.saveAll(Arrays.asList(person, person1, person2));
+            Page<Person> personPage1 = personRepository.search(queryByAgeRangeAndNameRegex);
+            Page<Person> personPage2 = personRepository.search(fuzzyQuery);
+            Page<Person> personPage3 = personRepository.search(anotherFuzzyQuery);
+
+            personPage1.forEach(System.out::println);
+            System.out.println("-------------------");
+            personPage2.forEach(System.out::println);
+            System.out.println("-------------------");
+            personPage3.forEach(System.out::println);
+            System.out.println("-------------------");
         };
     }
 
